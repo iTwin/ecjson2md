72e6320180b5b6adef9f3e8a9289eba15119953e
Jacob.Arnold <38288322+imodeljs-admin@users.noreply.github.com>
2018-06-29 10:54:09 -0400

    add more revisions

---
+++ test/ecjson2md_cli.test.ts
@@ -0,0 +1,18 @@
+/*---------------------------------------------------------------------------------------------
+|  $Copyright: (c) 2018 Bentley Systems, Incorporated. All rights reserved. $
+*--------------------------------------------------------------------------------------------*/
+
+import { prepOutputPath } from "../source/ecjson2md_cli";
+import { assert } from "chai";
+import * as path from "path";
+
+describe("ecjson2md_cli", () => {
+  describe("File path formatting", () => {
+    describe("Output file path formatting", () => {
+      it("should correctly format a path using /\'s", () => {
+        const preppedPath = prepOutputPath("dir1/dir2", "file.ecschema.json");
+        assert.equal(preppedPath, path.join("dir1", "dir2", "file.ecschema.json"));
+      });
+    });
+  });
+});
