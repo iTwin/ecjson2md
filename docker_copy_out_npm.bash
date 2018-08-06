fe5339797590b4bdead37eaf81655b698fac7744
Jacob Arnold - Intern <38288322+imodeljs-admin@users.noreply.github.com>
2018-08-06 11:46:17 -0400

    add script to copy out npm package

---
+++ docker_copy_out_npm.bash
@@ -0,0 +1,13 @@
+#!/bin/bash
+IMAGE_NAME=$1
+TARGET_PATH=$2
+
+# Get the container ID
+CONTAINER_ID=$(echo $(docker ps --no-trunc -aqf "ancestor=$IMAGE_NAME") | awk '{print $1}')
+
+# Echo path for debugging
+echo "Container ID:"
+echo "$CONTAINER_ID:$REPO_SOURCE_PATH"
+
+# Copy repo out of docker container
+docker cp "$CONTAINER_ID:/npm_package/" $TARGET_PATH
